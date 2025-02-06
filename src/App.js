75ae92df65e9fb02ae3f6b0918076c0832d55973
NompiloZondo-Dev <nompilo.n.zondo@gmail.com>
2025-02-06 15:04:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Meallist from './components/MealList';
+import { BasketProvider } from './BasketContext';
+import MealDetails from './components/MealDetails';
+import Checkout from './components/Checkout';
+import Payment from './components/Payment';
+import LandingPage from './components/LandingPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BasketProvider>
+      <Router>
+      <Routes>
+        {/* Landing page */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/mealList" element={<Meallist/>} />
+        {/* <Route path="/mealdetails/:id" element={<MealDetails />} /> */}
+        <Route path="/mealdetails/:mealId" element={<MealDetails />} /> {/* Meal detail page */}
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </Router>
+
+    </BasketProvider>
+
+
+    
   );
 }
 
